cdc56f0feacf65afaa9b3b8acc08c72e7765c1e6
Obsidian <ObsidianRock@users.noreply.github.com>
2017-03-27 20:10:50 +0100

    vector magnitude

--- datascience/4-vectors.py
+++ datascience/4-vectors.py
@@ -19,13 +19,24 @@ def vector_sum(vectors):
 
 #vector_sum(([1,2,5], [6,1,6]))  # 1+6, 2+1, 5+6
 
-def scalar_multiply(c, v)
+def scalar_multiply(c, v):
     return [c * v_i for v_i in v ]
 
 
 def vector_mean(vectors):
     n = len(vectors)
     return scalar_multiply(1/n , vector_sum(vectors))
+
+
+def dot(v, w):
+    return sum(v_i * w_i for v_i, w_i in zip(v, w))
+
+def sum_of_squares(v):
+    return dot(v, v)
+
+
+def magnitude(v):
+    return math.sqrt(sum_of_squares(v))
 
 
 if __name__ == '__main__':
