a86ae5a50b423366a036b5575cf6f8495b79f988
Obsidian <ObsidianRock@users.noreply.github.com>
2017-03-27 18:47:58 +0100

    vector addition and subtraction

---
+++ datascience/4-vectors.py
@@ -0,0 +1,16 @@
+
+
+
+def vector_add(v, w):
+
+    return [v_i + w_i for v_i, w_i in zip(v, w)]
+
+
+def vector_sub(v, w):
+    return  [v_i - w_i for v_i, w_i in zip(v, w)]
+
+
+
+if __name__ == '__main__':
+
+    pass
