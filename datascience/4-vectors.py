50317d88a35adc2fb97ccbbc6368e36f72060b7a
Obsidian <ObsidianRock@users.noreply.github.com>
2017-03-27 18:57:00 +0100

    componentwise sum a list of vectors

--- datascience/4-vectors.py
+++ datascience/4-vectors.py
@@ -10,6 +10,14 @@ def vector_sub(v, w):
     return  [v_i - w_i for v_i, w_i in zip(v, w)]
 
 
+def vector_sum(vectors):
+
+    result = vectors[0]
+    for vector in vectors[1:]:
+        result = vector_add(result, vector)
+    return result
+
+vector_sum(([1,2,5], [6,1,6]))  # 1+6, 2+1, 5+6
 
 if __name__ == '__main__':
 
