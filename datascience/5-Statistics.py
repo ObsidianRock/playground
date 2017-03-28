b29a3e84d30ab0d994e0507049dfdd04c9fe0b05
Obsidian <ObsidianRock@users.noreply.github.com>
2017-03-28 17:53:40 +0100

    variance and standard deviation

--- datascience/5-Statistics.py
+++ datascience/5-Statistics.py
@@ -1,3 +1,6 @@
+import math
+
+from 4-vectors import sum_of_squares
 
 def mean(x):
 
@@ -21,14 +24,42 @@ def median(x):
 
 
 def quantil(x, p):
-
     p_index = int(p * len(x))
     return sorted(x)[p_index]
 
 
-def mode(x):
+def mode(x)
 
-    count = Counter(x)
+    counts = Counter(x)
     max_count = max(counts.values())
 
-    return [x_i for x_i, count in counts.iteritems() if count = max_count]
+    return [x_i for x_i, count in counts.iteritems() if count == max_count]
+
+
+def data_range(x):
+
+    return max(x) - min(x)
+
+
+def de_mean(x):
+
+    x_bar = mean(x)
+
+    return [x_i - x_bar for x_i in x]
+
+
+def variance(x)
+
+    n = len(x)
+
+    deviations = de_mean(x)
+    
+    return sum_of_squares(deviation ) / (n -1 )
+
+def standard_deviation(x):
+
+    return math.sqrt(variance(x))
+
+def interquartile_range(x):
+
+    return quantile(x, 0.74) - quantile(x, 0.25)
