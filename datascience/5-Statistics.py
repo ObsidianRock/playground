9cb896955162cd18ceb42ec3cef01038d1e37a11
Obsidian <ObsidianRock@users.noreply.github.com>
2017-03-28 18:33:27 +0100

    correlation

--- datascience/5-Statistics.py
+++ datascience/5-Statistics.py
@@ -3,7 +3,6 @@ import math
 from 4-vectors import sum_of_squares
 
 def mean(x):
-
     return sum(x) / len(x)
 
 
@@ -32,34 +31,45 @@ def mode(x)
 
     counts = Counter(x)
     max_count = max(counts.values())
-
     return [x_i for x_i, count in counts.iteritems() if count == max_count]
 
 
 def data_range(x):
-
     return max(x) - min(x)
 
 
 def de_mean(x):
 
     x_bar = mean(x)
-
     return [x_i - x_bar for x_i in x]
 
 
 def variance(x)
 
     n = len(x)
-
     deviations = de_mean(x)
-    
     return sum_of_squares(deviation ) / (n -1 )
 
 def standard_deviation(x):
-
     return math.sqrt(variance(x))
 
 def interquartile_range(x):
-
     return quantile(x, 0.74) - quantile(x, 0.25)
+
+
+def covariance(x, y):
+
+    n = len(x)
+    return dot(de_mean(x), de_mean(y)) / (n -1)
+
+
+def correlation(x, y):
+
+    stdev_x = standard_deviation(x)
+    stdev_y = standard_deviation(y)
+
+    if stdev_x and stdev_y > 0:
+        return covariance(x, y) / stdev_x / stdev_y
+
+    else:
+        return 0
